669bb8a63fe3c0d4cae77658a24bb4c6778a2b1b
NhatBangLe <91333510+NhatBangLe@users.noreply.github.com>
2024-08-02 21:36:25 +0700

    update: ProjectUpdateRequest has status, ProjectRepository has a new method. refactor: ProjectService

--- src/main/java/com/microservices/projectservice/dto/request/ProjectUpdateRequest.java
+++ src/main/java/com/microservices/projectservice/dto/request/ProjectUpdateRequest.java
@@ -1,10 +1,13 @@
 package com.microservices.projectservice.dto.request;
 
+import com.microservices.projectservice.constant.ProjectStatus;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
 public record ProjectUpdateRequest(String name,
                                    String description,
+                                   ProjectStatus status,
                                    LocalDate startDate,
                                    LocalDate endDate) implements Serializable {
 }
--- src/main/java/com/microservices/projectservice/service/ProjectService.java
+++ src/main/java/com/microservices/projectservice/service/ProjectService.java
@@ -1,5 +1,6 @@
 package com.microservices.projectservice.service;
 
+import com.microservices.projectservice.constant.ProjectStatus;
 import com.microservices.projectservice.dto.request.ProjectCreateRequest;
 import com.microservices.projectservice.dto.request.ProjectMemberRequest;
 import com.microservices.projectservice.dto.response.ProjectResponse;
@@ -12,9 +13,11 @@ import com.microservices.projectservice.repository.UserRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,17 +30,7 @@ public class ProjectService {
 
     public ProjectResponse getProject(@NotNull String projectId) throws NoEntityFoundException {
         var project = findProject(projectId);
-        var memberIds = project.getMembers().stream().map(User::getId).toList();
-
-        return new ProjectResponse(
-                projectId,
-                project.getName(),
-                project.getDescription(),
-                project.getStartDate(),
-                project.getEndDate(),
-                project.getOwner().getId(),
-                memberIds
-        );
+        return mapProjectToResponse(project);
     }
 
     public String createProject(@NotNull ProjectCreateRequest projectCreateRequest) throws IllegalAttributeException {
@@ -69,20 +62,27 @@ public class ProjectService {
         return projectRepository.save(projectBuilder.build()).getId();
     }
 
-    public void updateProject(@NotNull String projectId, @NotNull ProjectUpdateRequest projectCreateRequest)
+    public void updateProject(@NotNull String projectId, @NotNull ProjectUpdateRequest projectUpdateRequest)
             throws NoEntityFoundException, IllegalAttributeException {
         var project = findProject(projectId);
 
-        String name = projectCreateRequest.name(),
-                description = projectCreateRequest.description();
+        String name = projectUpdateRequest.name(),
+                description = projectUpdateRequest.description();
         if (name != null) {
             if (name.isBlank() || name.isEmpty()) throw new IllegalAttributeException("Project name cannot be empty");
             project.setName(name);
         }
         if (description != null) project.setDescription(description);
 
-        LocalDate startDate = projectCreateRequest.startDate(),
-                endDate = projectCreateRequest.endDate();
+        var status = projectUpdateRequest.status();
+        if (status != null) {
+            if (!Arrays.asList(ProjectStatus.values()).contains(status))
+                throw new IllegalAttributeException("Invalid project status");
+            project.setStatus(status);
+        }
+
+        LocalDate startDate = projectUpdateRequest.startDate(),
+                endDate = projectUpdateRequest.endDate();
         if (startDate != null) project.setStartDate(startDate);
         if (endDate != null) project.setEndDate(endDate);
         if ((startDate != null && startDate.isAfter(project.getEndDate()))
@@ -114,34 +114,62 @@ public class ProjectService {
         projectRepository.delete(project);
     }
 
+    public List<ProjectResponse> getAllOwnProjects(@NotNull String userId,
+                                                   @NotNull ProjectStatus status,
+                                                   @NotNull Integer pageNumber,
+                                                   @NotNull Integer pageSize)
+            throws IllegalAttributeException, NoEntityFoundException {
+        var pageable = createPageable(pageNumber, pageSize);
+        var user = findUser(userId);
+
+        return projectRepository.findAllByOwner(user, pageable).stream()
+                .filter(project -> project.getStatus().equals(status))
+                .map(this::mapProjectToResponse)
+                .toList();
+    }
+
+    public List<ProjectResponse> getAllJoinProjects(@NotNull String userId,
+                                                    @NotNull ProjectStatus status,
+                                                    @NotNull Integer pageNumber,
+                                                    @NotNull Integer pageSize)
+            throws IllegalAttributeException, NoEntityFoundException {
+        var pageable = createPageable(pageNumber, pageSize);
+        var user = findUser(userId);
+
+        return projectRepository.findAllByMembersContains(user, pageable).stream()
+                .filter(project -> project.getStatus().equals(status))
+                .map(this::mapProjectToResponse)
+                .toList();
+    }
+
     private Project findProject(@NotNull String projectId) throws NoEntityFoundException {
         return projectRepository.findById(projectId)
                 .orElseThrow(() -> new NoEntityFoundException("No project found with id: " + projectId));
     }
 
-    public List<ProjectResponse> getAllProjects(@NotNull String userId,
-                                                @NotNull Integer pageNumber,
-                                                @NotNull Integer pageSize)
-            throws IllegalAttributeException, NoEntityFoundException {
+    private User findUser(@NotNull String userId) throws NoEntityFoundException {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NoEntityFoundException("No user found with id: " + userId));
+    }
+
+    private Pageable createPageable(@NotNull Integer pageNumber, @NotNull Integer pageSize)
+            throws IllegalAttributeException {
         if (pageNumber < 0 || pageSize <= 0)
             throw new IllegalAttributeException("Invalid page number or page size");
-        var pageable = PageRequest.of(pageNumber, pageSize);
+        return PageRequest.of(pageNumber, pageSize);
+    }
 
-        var user = userRepository.findById(userId)
-                .orElseThrow(() -> new NoEntityFoundException("No user found with id: " + userId));
-
-        return projectRepository.findAllByOwner(user, pageable).stream().map(project -> {
-            var memberIds = project.getMembers().parallelStream().map(User::getId).toList();
-            return new ProjectResponse(
-                    project.getId(),
-                    project.getName(),
-                    project.getDescription(),
-                    project.getStartDate(),
-                    project.getEndDate(),
-                    user.getId(),
-                    memberIds
-            );
-        }).toList();
+    private ProjectResponse mapProjectToResponse(@NotNull Project project) {
+        var memberIds = project.getMembers().parallelStream().map(User::getId).toList();
+        return new ProjectResponse(
+                project.getId(),
+                project.getName(),
+                project.getDescription(),
+                project.getStartDate(),
+                project.getEndDate(),
+                project.getOwner().getId(),
+                memberIds
+        );
     }
 
 }
