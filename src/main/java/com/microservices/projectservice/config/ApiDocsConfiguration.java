8d360e8ea10a6f1e5d6bb71833a9df246618ab27
NhatBangLe <91333510+NhatBangLe@users.noreply.github.com>
2024-09-03 00:22:02 +0700

    Update API Docs configs

--- src/main/java/com/microservices/projectservice/config/ApiDocsConfiguration.java
+++ src/main/java/com/microservices/projectservice/config/ApiDocsConfiguration.java
@@ -2,26 +2,30 @@ package com.microservices.projectservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class ApiDocsConfiguration {
 
+    @Value("${API_DOCS_SERVER}")
+    private String API_DOCS_SERVER;
+
     @Bean
     public OpenAPI getOpenAPI() {
-        var info = new Info()
-                .title("Project Service")
-                .version("1.0")
-                .description(
-                        "This is a set of services which is developed for sample collecting projects using by Expert. " +
-                        "It supports to manage projects, forms, stages and samples."
-                );
-
-        var openApi = new OpenAPI();
-        openApi.setInfo(info);
-
-        return openApi;
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Project Service")
+                        .version("1.0")
+                        .description(
+                                "This is a set of services which is developed for sample collecting projects using by Expert. " +
+                                "It supports to manage projects, forms, stages and samples."
+                        ))
+                .servers(List.of(new Server().url(API_DOCS_SERVER)));
     }
 
 }
