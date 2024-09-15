c37bcfed9e93b903274d42d5f005cc9aa01c2a76
NhatBangLe <91333510+NhatBangLe@users.noreply.github.com>
2024-09-15 14:56:37 +0700

    Update CORS configuration and Expose APP_NAME variable for custom purpose

--- src/main/java/com/microservices/projectservice/config/WebConfig.java
+++ src/main/java/com/microservices/projectservice/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.microservices.projectservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,11 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .maxAge(3600);
     }
 
