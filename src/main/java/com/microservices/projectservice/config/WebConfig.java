eebf0c46f06046676daa3bcb1aeefbe89c16c9fe
NhatBangLe <91333510+NhatBangLe@users.noreply.github.com>
2024-09-15 15:20:22 +0700

    Update CORS configuration for allowing many gateway origins

--- src/main/java/com/microservices/projectservice/config/WebConfig.java
+++ src/main/java/com/microservices/projectservice/config/WebConfig.java
@@ -1,24 +1,40 @@
 package com.microservices.projectservice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${ALLOWED_ORIGIN}")
-    private String allowedOrigin;
+    private final DiscoveryClient discoveryClient;
+
+    @Value("${GATEWAY_INSTANCE_ID}")
+    private String gatewayId;
+
+    public List<String> gatewayUrls() {
+        var instances = discoveryClient.getInstances(gatewayId);
+        if (instances.isEmpty()) return List.of("*");
+
+        return instances.stream()
+                .map(instance -> instance.getUri().toString())
+                .toList();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin)
+        var registeredMapping = registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .maxAge(3600);
+        gatewayUrls().forEach(registeredMapping::allowedOrigins);
     }
 
 }
